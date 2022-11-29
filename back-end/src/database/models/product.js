3607a77be8131bf6006c85101cb123b4978e3f39
Pedro-28 <pedrohdp2017@gmail.com>
2022-11-29 16:44:17 -0300

    Implementa migrations, models e seeders

---
+++ back-end/src/database/models/product.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL(4, 2),
+    urlImage: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'products',
+  });
+
+  return Product;
+};
