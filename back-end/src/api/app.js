114da6b0367e1ab7a693c3de1623136d891c67bc
alissonRH <alisson.honorato@live.com>
2022-11-30 02:05:45 -0300

    adicionei o cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const errorMiddleware = require('./Middlewares/errorMiddleware');
 const routesLogin = require('./Routes/loginRoute');
+const cors = require('cors');
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 
